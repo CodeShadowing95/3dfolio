12d0559ea5584f2d95191258d1f1c89f9b2af2ea
VastoLorde <ksenbonsakura@gmail.com>
2024-01-15 16:04:32 +0100

    UPDATE: Hero component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
                 }}
             >
                 <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-                <p className="text-white text-[18px] font-bold cursor-pointer flex">Patrick NAMEGNI &nbsp; <span className="font-light sm:block hidden">| Fullstack Web Developer</span></p>
+                <p className="text-white text-[18px] font-bold cursor-pointer flex">Patrick &nbsp; <span className="font-light sm:block hidden">| Fullstack Web Developer</span></p>
             </Link>
 
             <ul className="list-none hidden sm:flex flex-row gap-10">
