f0544f80bae20bba131e957f168947479c770ce7
Patrick Namegni <49642047+CodeShadowing95@users.noreply.github.com>
2026-09-13 09:14:26 +0200

    refactor: Change logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
           to="/"
-          className="flex items-center gap-2"
+          className="flex items-center"
           onClick={() => {
             setActive("");
             window.scrollTo(0, 0);
@@ -50,7 +50,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Patrick
+            atrkTech
             {/* &nbsp; <span className="font-light sm:block hidden">| Fullstack Web Developer</span>*/}
           </p>
         </Link>
